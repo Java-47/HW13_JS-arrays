bb7085fdf3efe948385830eac0c8aca09ad7747f
YourMirror <kamenka993@gmail.com>
2022-04-25 00:05:48 +0300

    hw13

---
+++ script.js
@@ -0,0 +1,38 @@
+const primes = [2, 3, 5, 7, 11, 13, 17, 19];
+printArray(primes);
+reverseArray(primes);
+printArray(primes);//19, 17, 13, 11, 7, 5, 3, 2
+let index = search(13, primes);
+console.log(index); //2
+index = search(1, primes);
+console.log(index); //-1
+
+function reverseArray(arr) {
+    let save_num
+    for (let i = 0; i < arr.length/2; i++)
+    {
+        save_num = arr[i]
+        arr[i] = arr[arr.length-i-1]
+        arr[arr.length-i-1] = save_num
+        //console.log(arr[i]);
+    }
+
+}
+
+function search(item, arr) {
+    let index;
+    
+    for(i in arr){
+        arr[i] === item ? index = i : -1;
+    }
+
+    return index !== undefined ? index : -1 
+}
+
+function printArray(arr){
+    console.log('=====================');
+    for(let i = 0; i < arr.length; i++){
+        console.log(arr[i]);
+    }
+    console.log('=====================');
+}
